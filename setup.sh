32bb38df296ef8a2d39a73b4a9307b6d957f8d33
Domenico Gagliano <40624589+Zhal73@users.noreply.github.com>
2020-08-05 12:00:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # setup a mysql db in a docker container
 
 # install docker if it isn't already
-if ! sudo docker --version > /dev/null; then
+if sudo docker --version > /dev/null; then
     curl https://get.docker.com | sudo bash
 fi
 
